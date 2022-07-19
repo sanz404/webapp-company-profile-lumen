fd1a6133709da43de121d32b94deda452aba2ebd
Sandy Andryanto <sandyandryanto@gmail.com>
2022-07-19 08:48:47 +0700

    up

--- backend/database/seeds/DatabaseSeeder.php
+++ backend/database/seeds/DatabaseSeeder.php
@@ -20,25 +20,159 @@ use App\Models\Message;
 use App\Models\Notification;
 use App\Models\Product;
 use App\Models\Project;
-use App\Models\Setting;
 use App\Models\Team;
 use App\Models\User;
 
 
 class DatabaseSeeder extends Seeder
 {
+    const DEFAULT_ADMIN_USERNAME = "admin";
+    const DEFAULT_ADMIN_EMAIL = "admin@devel.com";
 
     public function run()
     {
-        // $this->createOthers();
-        // $this->createCountries();
-        // $this->createUsers();
-        // $this->createContacts();
-        // $this->createNotification();
-        // $this->creatArticles();
-        // $this->createContent();
-        // $this->createFaqs();
+        $this->command->warn('Begin instalation, please wait...');
+
+        $this->resetTable();
+        $this->createOthers();
+        $this->createCountries();
+        $this->createUsers();
+        $this->createContacts();
+        $this->createNotification();
+        $this->creatArticles();
+        $this->createContent();
+        $this->createFaqs();
         $this->createFeature();
+        $this->createMessage();
+        $this->createProduct();
+        $this->createProject();
+        $this->createTeam();
+
+        $admin = User::where("is_admin", 1)->first();
+        if(!is_null($admin)){
+
+            $this->command->info('-----------------------------------------');
+            $this->command->info('Admin Username : '.self::DEFAULT_ADMIN_USERNAME);
+            $this->command->info('Admin Email : '.self::DEFAULT_ADMIN_EMAIL);
+            $this->command->info('Admin Password : 5ecReT!');
+            $this->command->info('-----------------------------------------');
+        }
+
+        $this->command->warn('Instalaation has been finished :)');
+    }
+
+    private function resetTable(){
+        About::truncate();
+        Article::truncate();
+        ArticleComment::truncate();
+        CategoryArticle::truncate();
+        CategoryProduct::truncate();
+        CategoryProject::truncate();
+        CategoryFaq::truncate();
+        Content::truncate();
+        Contact::truncate();
+        Country::truncate();
+        EmailVerification::truncate();
+        Faq::truncate();
+        Feature::truncate();
+        Message::truncate();
+        Notification::truncate();
+        Product::truncate();
+        Project::truncate();
+        Team::truncate();
+        User::truncate();
+    }
+
+    private function createTeam(){
+
+        $faker = Faker::create();
+
+        $data = array(
+            array(
+                "name"=> "Ibbie Eckart",
+                "position"=> "Founder & CEO",
+                "description"=> $faker->text,
+                "is_published"=> 1
+            ),
+            array(
+                "name"=> "Arden Vasek",
+                "position"=> "CFO",
+                "description"=> $faker->text,
+                "is_published"=> 1
+            ),
+            array(
+                "name"=> "Toribio Nerthus",
+                "position"=> "Operations Manager",
+                "description"=> $faker->text,
+                "is_published"=> 1
+            ),
+            array(
+                "name"=> "Malvina Cilla",
+                "position"=> "CTO",
+                "description"=> $faker->text,
+                "is_published"=> 1
+            )
+        );
+
+        foreach($data as $row){
+            Team::create($row);
+        }
+    }
+
+    private function createProject(){
+        for($i = 1; $i <= 4; $i++){
+            $category = CategoryProject::inRandomOrder()->first();
+            Project::create([
+                "category_id"=> $category->id,
+                "name"=> "Project ".$i,
+                "description"=> "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.",
+                "is_published"=> 1
+            ]);
+        }
+    }
+
+    private function createProduct(){
+        $category = CategoryProduct::inRandomOrder()->first();
+        $data = array(
+            array(
+                "category_id"=> $category->id,
+                "name"=> "FREE",
+                "price"=> 0,
+                "description"=> "<ul><li><strong>1 users</strong></li><li>5GB storage</li><li>Unlimited public projects</li><li>Community access</li><li>Unlimited private projects</li><li>Dedicated support</li><li>Free linked domain</li><li>Monthly status reports</li></ul>",
+                "is_published"=> 1
+            ),
+            array(
+                "category_id"=> $category->id,
+                "name"=> "PRO",
+                "price"=> 9,
+                "description"=> "<ul><li><strong>5 users</strong></li><li>5GB storage</li><li>Unlimited public projects</li><li>Community access</li><li>Unlimited private projects</li><li>Dedicated support</li><li>Free linked domain</li><li>Monthly status reports</li></ul>",
+                "is_published"=> 1
+            ),
+            array(
+                "category_id"=> $category->id,
+                "name"=> "ENTERPRISE",
+                "price"=> 49,
+                "description"=> "<ul><li><strong>Unlimited users</strong></li><li>5GB storage</li><li>Unlimited public projects</li><li>Community access</li><li>Unlimited private projects</li><li>Dedicated support</li><li><strong>Unlimited</strong> linked domains</li><li>Monthly status reports</li></ul>",
+                "is_published"=> 1
+            )
+        );
+
+        foreach($data as $row){
+            Product::create($row);
+        }
+
+    }
+
+    private function createMessage(){
+        for($i = 1; $i <= 100; $i++){
+            $faker = Faker::create();
+            Message::create([
+                "full_name"=> $faker->name,
+                "email"=> $faker->email,
+                "phone"=> $faker->phoneNumber,
+                "message"=> $faker->text
+            ]);
+        }
     }
 
     private function createFeature(){
@@ -129,7 +263,7 @@ class DatabaseSeeder extends Seeder
             $faker = Faker::create();
             About::create([
                 "title"=> $row,
-                "decsription"=> $faker->text
+                "description"=> $faker->text
             ]);
         }
 
@@ -138,8 +272,8 @@ class DatabaseSeeder extends Seeder
         foreach($category_articles as $row){
             $faker = Faker::create();
             CategoryArticle::create([
-                "title"=> $row,
-                "decsription"=> $faker->text
+                "name"=> $row,
+                "description"=> $faker->text
             ]);
         }
 
@@ -148,8 +282,8 @@ class DatabaseSeeder extends Seeder
         foreach($category_faqs as $row){
             $faker = Faker::create();
             CategoryFaq::create([
-                "title"=> $row,
-                "decsription"=> $faker->text
+                "name"=> $row,
+                "description"=> $faker->text
             ]);
         }
 
@@ -158,8 +292,8 @@ class DatabaseSeeder extends Seeder
         foreach($category_products as $row){
             $faker = Faker::create();
             CategoryProduct::create([
-                "title"=> $row,
-                "decsription"=> $faker->text
+                "name"=> $row,
+                "description"=> $faker->text
             ]);
         }
 
@@ -168,8 +302,8 @@ class DatabaseSeeder extends Seeder
         foreach($category_products as $row){
             $faker = Faker::create();
             CategoryProject::create([
-                "title"=> $row,
-                "decsription"=> $faker->text
+                "name"=> $row,
+                "description"=> $faker->text
             ]);
         }
 
